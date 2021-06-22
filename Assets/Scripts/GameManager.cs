f2647ffc6e5db87a2d1ad0a2f8d545c4894cc978
Eric Oliver <eroliver27@hotmail.com>
2021-06-22 02:00:36 -0400

    added trigger detection for balls
    added my standard game manager and and scene loader scripts so that menu and levels can be added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+
+    private void Awake()
+    {
+        MakeSingleton();
+    }
+
+    void MakeSingleton()
+    {
+        if(gameManager != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            gameManager = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public event Action onNextSceneEnter;
+    public void NextSceneEnter()
+    {
+        if (onNextSceneEnter != null)
+        {
+            onNextSceneEnter();
+        }
+    }
+
+    public event Action onExitEnter;
+    public void ExitEnter()
+    {
+        if (onExitEnter != null)
+        {
+            onExitEnter();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            NextSceneEnter();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ExitEnter();
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void Start()
+    {
+        GameManager.gameManager.onNextSceneEnter += LoadNextScene;
+        GameManager.gameManager.onExitEnter += QuitGame;
+
+    }
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        if (currentSceneIndex == sceneCount - 1)
+        {
+            LoadStartScene();
+        }
+        else
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.gameManager.onNextSceneEnter -= LoadNextScene;
+    }
+}
